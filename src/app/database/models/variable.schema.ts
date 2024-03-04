4f2552bfa89d1b2374c2c9287c59fe05399b1b71
sarin32 <execode02@gmail.com>
2024-03-04 23:07:00 +0530

    [fix] variable schema optimisations

--- src/app/database/models/variable.schema.ts
+++ src/app/database/models/variable.schema.ts
@@ -1,10 +1,9 @@
 import {Document, ObjectId} from 'mongodb';
 
 export interface VariableSchema extends Document {
-  name: string;
   key: string;
   value: string;
   environment_id: ObjectId;
-  user_id: ObjectId;
+  user_id?: ObjectId;
   created_at: Date;
 }
