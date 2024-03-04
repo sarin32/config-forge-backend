1cf75670eeace70b9a205b21984b24fdfebcee2d
sarin32 <execode02@gmail.com>
2024-03-04 22:24:25 +0530

    [style] code formatting

--- src/app/database/models/index.ts
+++ src/app/database/models/index.ts
@@ -8,12 +8,18 @@ import {UserSchema} from './user.schema';
 import {VariableSchema} from './variable.schema';
 
 export const userModal = connection.getCollection<UserSchema>('users');
+
 export const emailVerificationModal =
   connection.getCollection<EmailVerificationSchema>('email_verifications');
+
 export const accessModal = connection.getCollection<AccessSchema>('access');
+
 export const projectModal = connection.getCollection<ProjectSchema>('projects');
+
 export const environmentModal =
   connection.getCollection<EnvironmentSchema>('environments');
+
 export const tokenModal = connection.getCollection<TokenSchema>('tokens');
+
 export const variableModal =
   connection.getCollection<VariableSchema>('variables');
